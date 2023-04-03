46d128acd3ec8bc07f5d958bd26e268cac8f7e08
shanmugapriya-anjane <shanmugapriya@anjanetech.com>
2023-04-03 17:18:06 +0530

    created dim_service

---
+++ snowflake/models/dimensions/dim_service.sql
@@ -0,0 +1,28 @@
+
+
+
+
+{{
+  config(
+    materialized='incremental',
+    unique_key = '"SERVICE_ID"',
+    merge_update_columns = [var("col_update_dts"),'SERVICE'],
+    tags = ["dimensions"],
+    schema = var("usage_data_datamart_schema_name")
+  )
+}}
+with
+ 
+dimensions AS
+              (select DISTINCT SERVICE
+              
+                             from {{ref('daily_spend')}})
+
+
+SELECT 
+     {{ dbt_utils.generate_surrogate_key(
+         ['"SERVICE"']
+     )}} as "SERVICE_ID", *,
+     current_timestamp as "{{var('col_create_dts')}}",
+        current_timestamp as "{{var('col_update_dts')}}"
+FROM dimensions
