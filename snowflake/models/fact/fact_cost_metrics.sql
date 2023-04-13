05a3b2d6ae4994bed22cffed12ebe5f4eb88c1d6
yuvarajan-anjane <102140287+yuvarajan-anjane@users.noreply.github.com>
2023-04-13 13:35:57 +0530

    fact_cost updated

--- snowflake/models/fact/fact_cost_metrics.sql
+++ snowflake/models/fact/fact_cost_metrics.sql
@@ -2,7 +2,7 @@
 {{
   config(
     materialized='incremental',
-    merge_update_columns = ['var("col_update_dts")','WAREHOUSE_ID','DATABASE_ID','SERVICE_ID','STORAGE_TYPE_ID'],
+    merge_update_columns = ['var("col_update_dts")','SPEND','SPEND_NET_CLOUD_SERVICES','CURRENCY'],
     tags = ["fact"],
     schema = var("usage_data_datamart_schema_name")
   )
@@ -11,18 +11,22 @@
 
 
 SELECT
-        daily_spend.date,
-        {{dbt_utils.generate_surrogate_key(["daily_spend.SERVICE"])}} "SERVICE_ID",
-        {{dbt_utils.generate_surrogate_key(["daily_spend.STORAGE_TYPE"])}} "STORAGE_TYPE_ID",
-        {{dbt_utils.generate_surrogate_key(["daily_spend.WAREHOUSE_NAME"])}} "WAREHOUSE_ID",
-        {{dbt_utils.generate_surrogate_key(["daily_spend.DATABASE_NAME"])}} "DATABASE_ID",
-        daily_spend."currency",
-        sum(daily_spend."SPEND"::DECIMAL(38, 5)) as "spend",
-        sum(daily_spend."SPEND_NET_CLOUD_SERVICES"::DECIMAL(38, 5)) as "spend_net_cloud_services",
+        ds."DATE",
+        {{dbt_utils.generate_surrogate_key(["ds.SERVICE"])}} "SERVICE_ID",
+        {{dbt_utils.generate_surrogate_key(["ds.STORAGE_TYPE"])}} "STORAGE_TYPE_ID",
+        {{dbt_utils.generate_surrogate_key(["ds.WAREHOUSE_NAME"])}} "WAREHOUSE_ID",
+        {{dbt_utils.generate_surrogate_key(["ds.DATABASE_NAME"])}} "DATABASE_ID",
+        ds."currency",
+        sum(ds."SPEND"::DECIMAL(38, 5)) as "spend",
+        sum(ds."SPEND_NET_CLOUD_SERVICES"::DECIMAL(38, 5)) as "spend_net_cloud_services",
         current_timestamp as "{{var('col_create_dts')}}",
         current_timestamp as "{{var('col_update_dts')}}"
- from  {{ref('daily_spend')}} daily_spend
- group by 1,2,3,4,5,6
-    
-    
-        
+ FROM  {{ref('daily_spend')}} ds
+ 
+ GROUP BY
+      ds."DATE",
+      ds."SERVICE",
+      ds."STORAGE_TYPE",
+      ds."WAREHOUSE_NAME",
+      ds."DATABASE_NAME",
+      ds."currency"
