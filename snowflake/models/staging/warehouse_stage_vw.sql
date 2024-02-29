3d0fe3c2c7d6dd073a95e298462456cc9d5087e8
shanm <shanmugapriya@anjanetech.com>
2024-02-29 11:14:02 +0530

    lightdash

--- snowflake/models/staging/warehouse_stage_vw.sql
+++
@@ -1,9 +0,0 @@
-{{config(materialized='view',
-        tags = ["staging"],
-        pre_hook = [generate_warehouse_metadata()],
-        schema = var("usage_data_staging_schema_name")
-        )
-}}
- 
- select *
- from {{source(var("source"),var("sf_tbl_warehouse"))}}
