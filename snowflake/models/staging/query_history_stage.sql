209f22e80b1b73650e127f04c68730ffb772d313
shanm <shanmugapriya@anjanetech.com>
2024-02-29 11:43:12 +0530

    ld-QH

--- snowflake/models/staging/query_history_stage.sql
+++ snowflake/models/staging/query_history_stage.sql
@@ -7,7 +7,7 @@
 }}
 
  select qh.*
- from {{source(var("source"),var("sf_tbl_query_history"))}} qh 
+ from snowflake.account_usage.query_history qh 
    inner join {{source(var("source"),var("sf_tbl_access_history"))}} ah on ah.QUERY_ID = qh.QUERY_ID
 {% if is_incremental() %}
  WHERE "END_TIME" > (SELECT max("{{var('col_update_dts')}}") FROM {{ref('usagedata_metadata')}} WHERE Source = 'staging' )
